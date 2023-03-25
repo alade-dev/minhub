c14691b69f0a77ac0cd6638e2fade2666339d243
Horlarmmy <aladetoheeb5@gmail.com>
2023-03-25 11:27:54 +0100

    Fix: Mint

--- frontend/pages/getstarted/form.js
+++ frontend/pages/getstarted/form.js
@@ -86,6 +86,7 @@ const Form = () => {
           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDdjMTVkRTM4NUU0Mzc1M0RBODNGZUE0NjgzZkZhMzc4RTFjZTUyZjEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2ODk3NjUxMTc3NCwibmFtZSI6IkRvY1QifQ.t7bF1OuxuS6S9QMP_rfl72fYMneOa1jzs-mZhdjEhog";
         const client = new NFTStorage({ token: NFT_STORAGE_TOKEN });
         const imageFile = new File(values.images, { type: "image/png" });
+        console.log(imageFile);
         const metadata = await client.store({
           name: values.name,
           description: values.description,
